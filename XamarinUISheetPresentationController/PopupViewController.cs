e5f1c75ab36b7e30adb1070befe2d934c50fdb03
Russell Garrett <russell.garrett@kloc.co.uk>
2022-04-12 12:23:39 +0100

    UISheetPresentationController implementation in Xamarin.IOS.

---
+++ XamarinUISheetPresentationController/PopupViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using UIKit;
+
+namespace XamarinUISheetPresentationController
+{
+    public partial class PopupViewController : UIViewController
+    {
+        public PopupViewController() : base("PopupViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            View.BackgroundColor = UIColor.Red;
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ XamarinUISheetPresentationController/ViewController.cs
@@ -0,0 +1,58 @@
+﻿using Foundation;
+using ObjCRuntime;
+using System;
+using UIKit;
+
+namespace XamarinUISheetPresentationController
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            
+            // https://matrejek.eu/posts/implementing-form-with-uisheetpresentationcontroller/
+            
+            NavigationItem.Title = "Sheet Demo";
+
+            var button = new UIBarButtonItem("Show Sheet", UIBarButtonItemStyle.Bordered, null);
+
+            button.Clicked += (s, e) =>
+            {
+                var popupViewController = new PopupViewController();
+                var navigationController = new UINavigationController(rootViewController: popupViewController);
+                navigationController.ModalPresentationStyle = UIModalPresentationStyle.PageSheet;
+                var sheetPresentationController = navigationController.PresentationController as UISheetPresentationController;
+                sheetPresentationController.PrefersGrabberVisible = true;
+                sheetPresentationController.Detents = new UISheetPresentationControllerDetent[]
+                {
+                    UISheetPresentationControllerDetent.CreateMediumDetent(),
+                    UISheetPresentationControllerDetent.CreateLargeDetent()
+                };
+
+                this.PresentViewController(navigationController, true, () =>
+                {
+
+                });
+            };
+
+            NavigationItem.RightBarButtonItem = button;
+        }
+
+
+        void ShowSheet()
+        {
+
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
